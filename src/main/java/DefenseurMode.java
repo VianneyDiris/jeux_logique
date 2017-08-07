3264d898cb115e80dc718268a2c60c112c8f5091
vianney diris <vianney.diris@gmail.com>
2017-08-07 16:46:53 +0200

    fin du mode défenseur pour le jeu recherche du code

--- src/main/java/DefenseurMode.java
+++ src/main/java/DefenseurMode.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,6 +10,7 @@ public class DefenseurMode {
 	ReaderFile reader = new ReaderFile();
 	int tabNombreJoueur[] = new int [reader.getNbCase()];
 	int tabNombreOrdinateur[] = new int [reader.getNbCase()];
+	String tabIndice[] = new String [reader.getNbCase()];
 	
 	//demande un nombre à l'utilisateur et l'enregistre dans un tableau
 	public void lecture() {
@@ -39,7 +41,56 @@ public class DefenseurMode {
 				}
 			System.out.println("");
 		}
-	
+		//demande les indices aux joueurs
+		public void indice () {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Réponse : ");
+			String indice = sc.nextLine();
+			tabIndice = indice.split("");
+						
+		}
+		
+		//change le numéro en fonction des indices
+		public void adapteNumero() {
+			for(int i = 0;i<reader.getNbCase();i++) {
+				if(tabIndice[i].equals("-")) {
+					tabNombreOrdinateur[i] = tabNombreOrdinateur[i] - 1;
+				}
+				else if(tabIndice[i].equals("+")) {
+					tabNombreOrdinateur[i] = tabNombreOrdinateur[i] + 1;
+				}
+				else {
+					tabNombreOrdinateur[i] = tabNombreOrdinateur[i];
+				}
+			}
+			for (int i = 0;i<reader.getNbCase();i++) {
+				System.out.print(tabNombreOrdinateur[i]);
+				}
+			
+		}
+		
+		public void partieDefenseur() {
+			lecture();
+			randomNumber();
+			int nbEssai = reader.getNbEssai();
+			while (!Arrays.equals(tabNombreJoueur, tabNombreOrdinateur) && nbEssai!=0) {
+						indice();
+						adapteNumero();						
+						nbEssai--;
+						System.out.println("\nIl reste "+nbEssai+" tours");
+											
+					}
+					
+			if (nbEssai==0) {
+				System.out.println("Vous avez gagné,le nombre d'essai est dépassé.");
+								
+			}
+			else {
+				System.out.println("Désolé mais vous avez perdu, l'ordinateur a trouvé votre numéro.");
+				
+			}
+			
+		}
 		
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ChallengerMode mode = new ChallengerMode();
+		//ChallengerMode mode = new ChallengerMode();
 		/*mode.randomNumber();
 		mode.lecture();
 		mode.comparerNombre();
 		*/
-		mode.partieChallenger();
+		//mode.partieChallenger();
+		DefenseurMode mode = new DefenseurMode();
+		mode.partieDefenseur();
 		
 		
 		
