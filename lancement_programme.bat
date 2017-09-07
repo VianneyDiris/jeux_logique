ab4908a2c4565c785a73d537c0a4b646ca141648
vianney diris <vianney.diris@gmail.com>
2017-09-07 15:12:01 +0200

    ajout de log4j.xml

--- lancement_programme.bat
+++ lancement_programme.bat
@@ -1,8 +1,8 @@
 cd ./src/main/java
-javac -classpath C:\Users\vianney\Documents\jeux_logique\lib\commons-lang3-3.6.jar *.java
+javac -classpath C:\Users\vianney\Documents\jeux_logique\lib\commons-lang3-3.6.jar;C:\Users\vianney\Documents\jeux_logique\lib\log4j-1.2.17.jar; *.java
 cd ..
 cd ..
-java -classpath C:\Users\vianney\Documents\jeux_logique\lib\commons-lang3-3.6.jar; main.java.Main
+java -classpath C:\Users\vianney\Documents\jeux_logique\lib\commons-lang3-3.6.jar;C:\Users\vianney\Documents\jeux_logique\lib\log4j-1.2.17.jar; main.java.Main
 
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,22 @@
 package main.java;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author vianney
  * @version 0.2
  *classe permettant de lancer le jeu
  */
 public class Main {
-
+	static Logger logger = Logger.getLogger(Main.class);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Menu menu = new Menu();
-		menu.menuJeu();
+//		Menu menu = new Menu();
+//		menu.menuJeu();
 	
 		
-		
+		logger.debug("test");
 		
 
 	}
